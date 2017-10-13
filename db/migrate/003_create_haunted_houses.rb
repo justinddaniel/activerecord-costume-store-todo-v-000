717cd9baca568832a852a2dc45351e0bc5999be2
Justin Daniel <justinddaniel@gmail.com>
2017-10-13 01:25:20 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,7 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 
   def change
-    create_table :hauntedhouses do |h|
+    create_table :haunted_houses do |h|
       h.string :name
       h.string :location
       h.string :theme
